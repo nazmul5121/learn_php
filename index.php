2d94b20db5667ad38e26a2dfaa50de639414dbb8
nazmul5121 <nazmul.khan121@gmail.com>
2023-11-09 11:14:43 +0600

    title and anchor

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-    echo 'hello'.'<br>';
-    $learn = "php";
-    echo "Learn ".$learn;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lern php</title>
+</head>
+<body>
+    <h3><?php echo $var ?></h3>
+    <?php 
+                                                                                  
+
+        //2. Write a PHP script to display the following strings.
+        echo "Tomorrow I 'll learn PHP global variables."."<br>"; 
+        echo "This is a bad command : del c:\\*.*"."\n";         
+        $var = 'PHP Tutorial';
+        
+
+
+    ?>                                                                          
+</body>
+</html>
---
+++ task3.php
@@ -0,0 +1,17 @@
+<?php
+    $var = 'PHP Tutorial'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $var ?></title>
+</head>
+<body>
+    <h3><?php echo $var ?></h3>
+    <p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
+    <p><a href="https://www.w3resource.com/php/php-home.php">Go to the <?php echo $var; ?></a>.</p>
+</body>
+</html>
