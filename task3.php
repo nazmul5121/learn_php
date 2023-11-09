436be1ed5f28866c97932f105b35e7e66203048f
nazmul5121 <nazmul.khan121@gmail.com>
2023-11-09 11:19:20 +0600

    title and anchor

--- task3.php
+++ task3.php
@@ -10,6 +10,9 @@
     <title><?php echo $var ?></title>
 </head>
 <body>
+    <!--3. $var = 'PHP Tutorial'. Put this variable into the title section, 
+    h3 tag and as an anchor text within an HTML document.-->
+
     <h3><?php echo $var ?></h3>
     <p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
     <p><a href="https://www.w3resource.com/php/php-home.php">Go to the <?php echo $var; ?></a>.</p>
