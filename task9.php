53f12c8f51baa3f807205a84da017d98a4cc84ea
nazmul5121 <nazmul.khan121@gmail.com>
2023-11-18 16:47:09 +0600

    accending & Decending

---
+++ task9.php
@@ -0,0 +1,21 @@
+<?php
+$x = array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+/*
+a) ascending order sort by value
+b) ascending order sort by Key
+c) descending order sorting by Value
+d) descending order sorting by Key
+*/
+ asort($x);
+ var_dump($x);
+ echo "<br>";
+ ksort($x);
+ var_dump($x);
+ echo "<br>";
+ arsort($x);
+ var_dump($x);
+ echo "<br>";
+ krsort($x);
+ var_dump($x);
+ echo "<br>";
+ print_r($x);
