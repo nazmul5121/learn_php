5f4e548b5cae38c26a1ebcd7989156233b7fe4d8
nazmul5121 <nazmul.khan121@gmail.com>
2023-11-19 10:56:32 +0600

    avarage value find out

---
+++ task10.php
@@ -0,0 +1,12 @@
+<?php
+    $temp = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+    $avarage = array_sum($temp)/count($temp);
+    echo $avarage;
+    echo "<br>";
+    echo "lowest five temparature in array: ";
+    sort($temp);
+    for ($i=0; $i < 5; $i++) 
+        {
+            echo $temp[$i], ", ";
+        }
+        
