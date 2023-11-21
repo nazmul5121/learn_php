a310b2000399dbb8073846309a1467b5b4011555
nazmul5121 <nazmul.khan121@gmail.com>
2023-11-21 22:11:13 +0600

    loop steaping

---
+++ task16.php
@@ -0,0 +1,9 @@
+<?php
+    // for loop steping
+    for($i=10; $i>0; $i--){
+        echo "$i\n";
+    }
+    echo "\n";
+    for($i=10,$j=1,$k=1,$l=10; $i>0; $i--,$j++,$k++,$l--){
+        echo "$i:$j                         $k:$l\n";
+    }
